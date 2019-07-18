f291c3fefe020121f03a853cab37485698763815
Jonathan Sant <jonathan.sant@live.co.uk>
2019-07-18 07:55:43 +0200

    feat(support dir): change `schemaRegistryUrl` to `schemaPath` to support setting the path of a directory (#1)
    * feat(support dir): change `schemaRegistryUrl` to `schemaPath` to support setting the path of a directory

--- Sucrose.Avro.CodeGen/Program.cs
+++ Sucrose.Avro.CodeGen/Program.cs
@@ -1,25 +1,24 @@
 ﻿using Confluent.SchemaRegistry;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Sucrose.Avro.CodeGen
 {
-	using global::Avro;
-
 	class Program
 	{
 		/// <summary>
 		/// avromagic auto-magically generates C# classes from an Avro Schema Registry.
 		/// </summary>
-		/// <param name="registryUrl">The Schema Registry URl</param>
+		/// <param name="schemaPath">The schema registry URL or File Path</param>
 		/// <param name="subjectPattern">Regex pattern to determine which schemas to retrieve</param>
 		/// <param name="outputDir">Output directory where to output the generated C# classes</param>
 		/// <param name="namespaceMapping">Map namespace from Producer's to Consumer's e.g. com.user:Sucrose.User</param>
 		static async Task<int> Main(
-			string registryUrl,
+			string schemaPath,
 			string subjectPattern = ".*",
 			string outputDir = ".",
 			string[] namespaceMapping = null
@@ -27,41 +26,9 @@ namespace Sucrose.Avro.CodeGen
 		{
 			try
 			{
-				var registry = new CachedSchemaRegistryClient(
-					new SchemaRegistryConfig
-					{
-						SchemaRegistryUrl = registryUrl
-					});
+				var schemas = await GetSchemas(schemaPath, subjectPattern);
 
-				var schemaPromises = (await registry.GetAllSubjectsAsync())
-					.Where(subject => Regex.IsMatch(subject, subjectPattern))
-					.Select(async subject => await registry.GetLatestSchemaAsync(subject));
-
-				var schemas = await Task.WhenAll(schemaPromises);
-				await Task.WhenAll(
-					schemas.Select(schema => GenerateClasses(schema.SchemaString, outputDir, namespaceMapping))
-				);
-
-				return 0;
-			}
-			catch (Exception ex)
-			{
-				Console.WriteLine(ex.Message);
-				return -1;
-			}
-		}
-
-		private static Task GenerateClasses(
-			string rawSchema,
-			string outputDir,
-			IEnumerable<string> namespaceMapping
-		) => Task.Run(() =>
-			{
-				var codeGen = new CodeGen();
-				var schema = Schema.Parse(rawSchema);
-
-				codeGen.AddSchema(schema);
-
+				var codeGen = new global::Avro.CodeGen();
 				if (namespaceMapping != null)
 				{
 					foreach (var mapping in namespaceMapping)
@@ -71,9 +38,54 @@ namespace Sucrose.Avro.CodeGen
 					}
 				}
 
+				await Task.WhenAll(
+					schemas.Select(schema => ParseSchemas(codeGen, schema))
+				);
+
 				codeGen.GenerateCode();
 				codeGen.WriteTypes(outputDir);
+
+				return 0;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"ERROR: {ex.Message}");
+				return -1;
+			}
+		}
+
+		private static Task ParseSchemas(
+			global::Avro.CodeGen codeGen,
+			string rawSchema
+		) => Task.Run(() =>
+			{
+				var schema = global::Avro.Schema.Parse(rawSchema);
+				codeGen.AddSchema(schema);
 			});
 
+		private static async Task<IEnumerable<string>> GetSchemas(string schemaPath, string subjectPattern = ".*")
+		{
+			if (!schemaPath.StartsWith("http"))
+				return await Task.WhenAll(Directory
+							.GetFiles(schemaPath)
+							.Where(path => Regex.IsMatch(path, subjectPattern))
+							.Select(path => File.ReadAllTextAsync(path)))
+							.ContinueWith(schemaPromise => schemaPromise.Result);
+
+			var registry = new CachedSchemaRegistryClient(
+				new SchemaRegistryConfig
+				{
+					SchemaRegistryUrl = schemaPath
+				});
+
+			var promises = await registry.GetAllSubjectsAsync()
+				.ContinueWith(subjectPromise => subjectPromise.Result
+					.Where(subject => Regex.IsMatch(subject, subjectPattern))
+					.Select(async subject => await registry.GetLatestSchemaAsync(subject))
+				);
+
+			return await Task.WhenAll(promises)
+				.ContinueWith(schemaPromise => schemaPromise.Result.Select(schema => schema.SchemaString));
+		}
 	}
 }
